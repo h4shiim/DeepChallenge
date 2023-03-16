ef89a3d9d5856abe53658f3b427fbe522d2bee26
Hashim <ahmedhashim.rn@gmail.com>
2023-03-16 18:45:20 +0200

    Modified tracks page and profile

--- client/src/components/Header/Navigations/Challenges/PrivateChallenges.jsx
+++ client/src/components/Header/Navigations/Challenges/PrivateChallenges.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './ProgrammingTracks.css';
 
-function PrivateChallenges() {
-  const navigate = useNavigate();
-
-  const handleChallengeClick = () => {
-    // Navigate to the challenge page
-    navigate('/challenges');
-  };
-
+const ProgrammingTracks = () => {
   return (
-    <div>
-      <h1>Private Challenges</h1>
-      <p>This is a private challenges page.</p>
-      <button onClick={handleChallengeClick}>Go to Challenges</button>
+    <div className="programming-tracks-container">
+      <h1 className="programming-tracks-header">Choose your programming track</h1>
+      <Link to="/tracks" className="programming-track-link">
+        <div className="programming-track">
+          <div className="programming-track-content">
+            <h2 className="programming-track-title">Web development</h2>
+            <p className="programming-track-description">Learn to build websites and web applications.</p>
+          </div>
+          <div className="programming-track-status">
+            <span className="programming-track-available">Available</span>
+          </div>
+        </div>
+      </Link>
+      <div className="programming-track">
+        <div className="programming-track-content">
+          <h2 className="programming-track-title">Game development</h2>
+          <p className="programming-track-description">Learn to build games for different platforms.</p>
+        </div>
+        <div className="programming-track-status">
+          <span className="programming-track-coming-soon">Coming soon</span>
+        </div>
+      </div>
+      <div className="programming-track">
+        <div className="programming-track-content">
+          <h2 className="programming-track-title">Mobile app development</h2>
+          <p className="programming-track-description">Learn to build apps for iOS and Android.</p>
+        </div>
+        <div className="programming-track-status">
+          <span className="programming-track-coming-soon">Coming soon</span>
+        </div>
+      </div>
+      <div className="programming-track">
+        <div className="programming-track-content">
+          <h2 className="programming-track-title">Fundamentals</h2>
+          <p className="programming-track-description">Learn the basics of programming.</p>
+        </div>
+        <div className="programming-track-status">
+          <span className="programming-track-available">Available</span>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default PrivateChallenges;
+export default ProgrammingTracks;
--- client/src/components/Header/Navigations/Register/Profile.jsx
+++ client/src/components/Header/Navigations/Register/Profile.jsx
@@ -1,6 +1,30 @@
 import React, { useState, useEffect } from 'react';
+import { CircularProgress, Grid, Paper, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    marginBottom: theme.spacing(2),
+  },
+  pointCounter: {
+    backgroundColor: '#000',
+    color: '#fff',
+    padding: theme.spacing(1),
+    borderRadius: theme.spacing(1),
+    textAlign: 'center',
+    marginBottom: theme.spacing(2),
+  },
+  progress: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '60vh',
+  },
+}));
 
 const Profile = () => {
+  const classes = useStyles();
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -21,35 +45,70 @@ const Profile = () => {
         setLoading(false);
       } catch (err) {
         console.error(err);
+        setError('Error fetching profile data');
         setLoading(false);
       }
     };
     fetchUser();
   }, []);
+  
 
   if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div>
-      <h1>Welcome, {user.username}!</h1>
-      <p>Email: {user.email}</p>
-      <p>Points: {user.points}</p>
-      <div>
-        <h2>Dashboard</h2>
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div>
-            <h3>Recent Activity</h3>
-            {/* TODO: display recent activity */}
-          </div>
-          <div>
-            <h3>Leaderboard</h3>
-            {/* TODO: display leaderboard */}
-          </div>
-        </div>
+    return (
+      <div className={classes.progress}>
+        <CircularProgress />
       </div>
-    </div>
+    );
+  }
+  
+  if (error) {
+    return <div>{error}</div>;
+  }
+  
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12} sm={6}>
+        <Paper className={classes.paper}>
+          <Typography variant="h5" component="h2" gutterBottom>
+            Profile
+          </Typography>
+          <Typography variant="h6" component="p" gutterBottom>
+            Name: {user.name}
+          </Typography>
+          <Typography variant="h6" component="p" gutterBottom>
+            Email: {user.email}
+          </Typography>
+          <Typography variant="h6" component="p" gutterBottom>
+            Points: {user.points}
+          </Typography>
+        </Paper>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Paper className={classes.paper}>
+          <Typography variant="h5" component="h2" gutterBottom>
+            Progress
+          </Typography>
+          <Typography variant="h6" component="p" gutterBottom>
+            Course Progress: {user.courseProgress}%
+          </Typography>
+          <Typography variant="h6" component="p" gutterBottom>
+            Achievements:
+          </Typography>
+          <ul>
+            {user.achievements.map((achievement) => (
+              <li key={achievement}>{achievement}</li>
+            ))}
+          </ul>
+        </Paper>
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.pointCounter}>
+          <Typography variant="h6" component="p" gutterBottom>
+            Points: {user.points}
+          </Typography>
+        </Paper>
+      </Grid>
+    </Grid>
   );
 };
 
